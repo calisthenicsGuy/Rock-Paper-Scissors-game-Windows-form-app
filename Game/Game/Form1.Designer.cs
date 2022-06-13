f8b0371f265460db54399ee573a0f29dcf7e8331
calisthenicsGuy <radevradoslav12@gmail.com>
2022-06-13 22:48:09 +0300

    game code added

---
+++ Game/Game/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblRounds = new System.Windows.Forms.Label();
+            this.picturieForPlayer = new System.Windows.Forms.PictureBox();
+            this.picturieForPC = new System.Windows.Forms.PictureBox();
+            this.winsAndLoses = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnRolls = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picturieForPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturieForPC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRock
+            // 
+            this.btnRock.BackColor = System.Drawing.Color.SlateGray;
+            this.btnRock.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRock.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnRock.Location = new System.Drawing.Point(30, 109);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(85, 29);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Text = "Камък";
+            this.btnRock.UseVisualStyleBackColor = false;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.BackColor = System.Drawing.Color.SlateGray;
+            this.btnPaper.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPaper.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnPaper.Location = new System.Drawing.Point(30, 254);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(85, 29);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.Text = "Хартия";
+            this.btnPaper.UseVisualStyleBackColor = false;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.BackColor = System.Drawing.Color.SlateGray;
+            this.btnScissors.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnScissors.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnScissors.Location = new System.Drawing.Point(30, 178);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(85, 29);
+            this.btnScissors.TabIndex = 2;
+            this.btnScissors.Text = "Ножица";
+            this.btnScissors.UseVisualStyleBackColor = false;
+            this.btnScissors.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTitle.Location = new System.Drawing.Point(238, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(304, 24);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Игра: Камък, Ножица, Хартия";
+            // 
+            // lblRounds
+            // 
+            this.lblRounds.AutoSize = true;
+            this.lblRounds.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblRounds.Location = new System.Drawing.Point(401, 264);
+            this.lblRounds.Name = "lblRounds";
+            this.lblRounds.Size = new System.Drawing.Size(64, 19);
+            this.lblRounds.TabIndex = 4;
+            this.lblRounds.Text = "Рунд:  1";
+            this.lblRounds.Click += new System.EventHandler(this.lblRounds_Click);
+            // 
+            // picturieForPlayer
+            // 
+            this.picturieForPlayer.Image = global::WindowsFormsApplication1.Properties.Resources.myQ;
+            this.picturieForPlayer.Location = new System.Drawing.Point(155, 109);
+            this.picturieForPlayer.Name = "picturieForPlayer";
+            this.picturieForPlayer.Size = new System.Drawing.Size(186, 174);
+            this.picturieForPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturieForPlayer.TabIndex = 5;
+            this.picturieForPlayer.TabStop = false;
+            // 
+            // picturieForPC
+            // 
+            this.picturieForPC.Image = global::WindowsFormsApplication1.Properties.Resources.myQ;
+            this.picturieForPC.Location = new System.Drawing.Point(531, 109);
+            this.picturieForPC.Name = "picturieForPC";
+            this.picturieForPC.Size = new System.Drawing.Size(186, 174);
+            this.picturieForPC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturieForPC.TabIndex = 6;
+            this.picturieForPC.TabStop = false;
+            // 
+            // winsAndLoses
+            // 
+            this.winsAndLoses.AutoSize = true;
+            this.winsAndLoses.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.winsAndLoses.Location = new System.Drawing.Point(392, 109);
+            this.winsAndLoses.Name = "winsAndLoses";
+            this.winsAndLoses.Size = new System.Drawing.Size(86, 19);
+            this.winsAndLoses.TabIndex = 7;
+            this.winsAndLoses.Text = "Играч: PC:";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTimer.Location = new System.Drawing.Point(461, 188);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(17, 19);
+            this.lblTimer.TabIndex = 9;
+            this.lblTimer.Text = "5";
+            // 
+            // btnRolls
+            // 
+            this.btnRolls.BackColor = System.Drawing.Color.SlateGray;
+            this.btnRolls.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRolls.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnRolls.Location = new System.Drawing.Point(3, 395);
+            this.btnRolls.Name = "btnRolls";
+            this.btnRolls.Size = new System.Drawing.Size(85, 29);
+            this.btnRolls.TabIndex = 10;
+            this.btnRolls.Text = "Правила";
+            this.btnRolls.UseVisualStyleBackColor = false;
+            this.btnRolls.Click += new System.EventHandler(this.btnRolls_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.SlateGray;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnExit.Location = new System.Drawing.Point(667, 395);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(85, 29);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(399, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 19);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Таймер:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ClientSize = new System.Drawing.Size(755, 427);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRolls);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.winsAndLoses);
+            this.Controls.Add(this.picturieForPC);
+            this.Controls.Add(this.picturieForPlayer);
+            this.Controls.Add(this.lblRounds);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Name = "Form1";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picturieForPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturieForPC)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblRounds;
+        private System.Windows.Forms.PictureBox picturieForPlayer;
+        private System.Windows.Forms.PictureBox picturieForPC;
+        private System.Windows.Forms.Label winsAndLoses;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnRolls;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Game/Game/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public int rounds = 10;
+        public long timePerRound = 6; 
+        string[] PCchoice = { "rock", "paper", "scissor", "rock", "scissor", "paper" };
+        public int randomNumber;
+        string command;
+        Random rnd = new Random();
+        string playerChoice;
+        int playerWins = 0;
+        int PCWins = 0;
+
+        int counterOfRounds = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Enabled = true;
+            playerChoice = "none";
+            lblTimer.Text = "5";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            picturieForPlayer.Image = Properties.Resources.rock;
+            playerChoice = "rock";
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            picturieForPlayer.Image = Properties.Resources.scissors;
+            playerChoice = "scissor";
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            picturieForPlayer.Image = Properties.Resources.paper;
+            playerChoice = "paper";
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            timePerRound--;
+            lblTimer.Text = Convert.ToString(timePerRound);
+            if (timePerRound < 1)
+            {
+
+                timer.Enabled = false; 
+
+                timePerRound = 6;
+                randomNumber = rnd.Next(0, 5);
+                command = PCchoice[randomNumber];
+                switch (command)
+                {
+                    case "rock":
+                        picturieForPC.Image = Properties.Resources.rock;
+                        break;
+                    case "paper":
+                        picturieForPC.Image = Properties.Resources.paper;
+                        break;
+                    case "scissor":
+                        picturieForPC.Image = Properties.Resources.scissors;
+                        break;
+                    default:
+                        break;
+                }
+
+                if (rounds > 0)
+                {
+                    checkGame();
+                }
+                else
+                {
+                    decisionEngine();
+                }
+            }
+        }
+
+        private void checkGame()
+        {
+            if (playerChoice == "rock" && command == "paper")
+            {
+                MessageBox.Show("PC спечели този рунд. :(");
+                PCWins++;
+                rounds--;
+                counterOfRounds++;
+                nextRound();
+            }
+            else if (playerChoice == "paper" && command == "rock")
+            {
+                MessageBox.Show("Ураа. Ти спечели този рунд!");
+                playerWins++;
+                rounds--;
+                counterOfRounds++;
+                nextRound();
+            }
+            else if (playerChoice == "paper" && command == "scissor")
+            {
+                MessageBox.Show("PC спечели този рунд. :(");
+                PCWins++;
+                rounds--;
+                counterOfRounds++;
+                nextRound();
+            }
+            else if (playerChoice == "scissor" && command == "paper")
+            {
+                MessageBox.Show("Ураа. Ти спечели този рунд!");
+                playerWins++;
+                rounds--;
+                counterOfRounds++;
+                nextRound();
+            }
+            else if (playerChoice == "scissor" && command == "rock")
+            {
+                MessageBox.Show("PC спечели този рунд. :(");
+                PCWins++;
+                rounds--;
+                counterOfRounds++;
+                nextRound();
+            }
+            else if (playerChoice == "rock" && command == "scissor")
+            {
+                MessageBox.Show("Ураа. Ти спечели този рунд!");
+                playerWins++;
+                rounds--;
+                counterOfRounds++;
+                nextRound();
+            }
+            else if (playerChoice == "none")
+            {
+                MessageBox.Show("Упсс ти не направи своя избор. Опитай пак като направиш своя избор!");
+                nextRound();
+            }
+            else
+            {
+                MessageBox.Show("В този рунд сте равни.");
+                counterOfRounds++;
+                nextRound();
+            }
+
+            if (counterOfRounds == 11)
+            {
+                lblTimer.Visible = false;
+                timePerRound = 1000000000;
+                lblTimer.Text = Convert.ToString(timePerRound);
+
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("Играта приключи!");
+                sb.AppendLine("");
+                sb.AppendLine("Резултатите от играта:");
+                sb.AppendLine("Твоите спечелени рундове: " + playerWins);
+                sb.AppendLine("Спечелените рундове на PC-то: " + PCWins);
+                sb.AppendLine("Това означава че....");
+                sb.AppendLine("");
+
+                if (playerWins > PCWins)
+                {
+                    sb.AppendLine("Поздравления ти спчечели!");
+                }
+                else if (PCWins > playerWins)
+                {
+                    sb.AppendLine("За съжеление PC-то спчечели! Опитай пак. :)");
+                }
+                else if (PCWins == playerWins)
+                {
+                    sb.AppendLine("Играта завърши наравно! Опитай пак! :)");
+                }
+
+                MessageBox.Show(sb.ToString());
+                this.Close();
+            }
+
+            lblRounds.Text = "Рунд: " + counterOfRounds;
+        }
+
+        private void decisionEngine()
+        {
+            if (playerWins > PCWins)
+            {
+                winsAndLoses.Text = winsAndLoses.Text + " спечели играта";
+            }
+            else
+            {
+                winsAndLoses.Text = "PC спчелеи играта";
+            }
+        }
+
+        private void nextRound()
+        {
+            playerChoice = "none";
+            picturieForPlayer.Image = Properties.Resources.myQ;
+            timer.Enabled = true;
+            picturieForPC.Image = Properties.Resources.myQ;
+            picturieForPC.Image = Properties.Resources.myQ;
+
+            winsAndLoses.Text = "Играч: " + playerWins + " - " + "PC: " + PCWins;
+        }
+
+        private void btnRolls_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("От посочените бутони играча трябва да избере един от трите хода.");
+            sb.AppendLine("Времето за избор е 5 секунди.");
+            sb.AppendLine("След избора на играча се генерира избор и на компютъра на случаен принцип.");
+            sb.AppendLine("Ако играча не направи избор рундовете не се увеличават.");
+            sb.AppendLine("След това се избира победител на база на ходовете.");
+            sb.AppendLine("В реално време се визуализират точките на играча и PC-то, както и оставащите рундове.");
+            sb.AppendLine("Рундовете са 10 на брой.");
+
+            MessageBox.Show(sb.ToString());
+        }
+
+        private void lblRounds_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
